709f1858693b5c489f47a318513c34e24a618f87
Yu Zhu <yu.zhu@zhusmelb.com>
2022-03-18 22:50:41 +1100

    init commit

---
+++ WebSiteTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using NUnit.Framework;
+
+namespace carved_rock_website_test;
+
+[TestFixture]
+public class WebSiteTest
+{
+    private HttpClient? _httpClient { get; set; } = null;
+
+    [SetUp]
+    public void Setup()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    [TestCase("")]
+    [TestCase("contact")]
+    public async Task Homepage_Ok_Response_Code(string relativePath)
+    {
+        HttpResponseMessage? response = null;
+        if (_httpClient is not null)
+        {
+            response = await _httpClient.GetAsync($"http://3.86.39.26/{relativePath}");
+        }
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response?.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+}
